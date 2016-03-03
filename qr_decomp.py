d451a12ac9f9fb2118ecbb3287b7a2df14dd2042
Abraao Mota <abraaomot@outlook.com>
2016-03-03 22:38:23 +0000

    Starting to mess with argparse
    Signed-off-by: Abraao Mota <abraaomot@outlook.com>

--- qr_decomp.py
+++ qr_decomp.py
@@ -1,23 +1,40 @@
 import random
+import argparse
 
-# Let a matrix of m rows and n columns be represented by m lists of n items
-
-
-def create_symmetric_matrix(rows, columns):
+def create_symmetric_matrix(dimension):
 	matrix = []
 	columns_covered = 0
 
-	for r in range (0, rows):
+	for r in range (0, dimension):
 		row = []
-		for c in range (0, columns):
+		for c in range (0, dimension):
 			if columns_covered > 0 and c < columns_covered:
 				row.append(matrix[c][r])
 			else:
-				row.append(random.random())
+				row.append(random.random() * random.randint(-100000, 100000))
 		matrix.append(row)
 		columns_covered += 1
 
 	return matrix
 
-matrix = create_symmetric_matrix(2,2)
-print(matrix)
+
+
+
+
+
+
+################################
+
+if __name__ == "__main__":
+	matrix = create_symmetric_matrix(2)
+	print(matrix)	
+
+	parser = argparse.ArgumentParser(description='Choose file or dimension of matrix')
+	parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                   help='an integer for the accumulator')
+	parser.add_argument('--sum', dest='accumulate', action='store_const',
+                   const=sum, default=max,
+                   help='sum the integers (default: find the max)')
+
+	args = parser.parse_args()
+	print args.accumulate(args.integers)
