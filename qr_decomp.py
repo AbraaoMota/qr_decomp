9ae2962571ce1e595db6d36547f932f6620e60ee
ap2314 <ap2314@ic.ac.uk>
2016-03-04 19:45:28 +0000

    Transpose and multiply seem to work, need further testing
    Signed-off-by: ap2314 <ap2314@ic.ac.uk>

--- qr_decomp.py
+++ qr_decomp.py
@@ -3,6 +3,46 @@ import sys
 import os
 import argparse
 
+#======================================================================
+# Matrix function utilities
+def transpose(m):
+	mT = []
+	for column in range (0, len(m[0])):
+		rowT = []
+		for row in range (0, len(m)):
+			rowT.append(m[row][column])
+		mT.append(rowT)
+	return mT
+
+#def inverse(m):
+
+def multiply(m1, m2):
+	m3 = []
+	for row in range (0, len(m1[0])):
+		row3 = []
+		for column in range (0, len(m1)):
+			row3.append(m1[row][column] * m2[row][column])
+		m3.append(row3)
+	return m3
+
+
+# def sum(m1, m2):
+
+#def determinant(m):
+	#if len(m) == 2:
+		#return ((m[0][0] * m[1][1]) - (m[0][1] * m[1][0]))
+	#else:
+
+
+# def normalise(v):
+
+# def dot_product(m1, m2):
+
+
+
+
+#======================================================================
+# Random symmetric matrix generator. Generates random floats in the range -100000, 100000
 def create_symmetric_matrix(dimension):
 	matrix = []
 	columns_covered = 0
@@ -66,9 +106,28 @@ def main(argv=None):
 	else:
 		matrix = create_symmetric_matrix(dimension)
 
+	
 	# Print the matrix
 	for i in range (0,dimension):
 		print(matrix[i])
+	
+	# Print determinant of matrix
+	# t = determinant(matrix)
+	# print("")
+	# print(t)
+
+
+	# Print transpose of matrix
+	t = transpose(matrix)
+	print("")
+	for i in range (0,dimension):
+		print(t[i])
+
+	# Print multiplication of matrices
+	m3 = multiply(matrix, t)
+	print("")
+	for i in range (0,dimension):
+		print(m3[i])		
 
 # ==============================================================================
 # call main if executed as script
